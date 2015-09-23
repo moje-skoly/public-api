7ec719399e0419604a03351c7516bcceb9eff5a4
Simon Rozsival <simon@rozsival.com>
2015-09-23 18:51:40 +0200

    Testing setup fixed

---
+++ test/build/mocha-test.js
@@ -0,0 +1,8 @@
+/// <reference path="../../typings/tsd.d.ts" />
+/**
+ * This is a totally dummy test which make sure the
+ * testing environment is configured well and works.
+ */
+describe("Mocha setup", function () {
+    it("shoud be ok", function (done) { return done(); });
+});
---
+++ test/src/mocha-test.ts
@@ -0,0 +1,11 @@
+/// <reference path="../../typings/tsd.d.ts" />
+
+
+/**
+ * This is a totally dummy test which make sure the 
+ * testing environment is configured well and works. 
+ */
+
+describe("Mocha setup", () => {
+	it("shoud be ok", (done) => done());
+});
